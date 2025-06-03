ca24cfe731d1da27ab0723581dba5dd281927cd8
Filipe Junqueiro <144694602+filipejunqueiro@users.noreply.github.com>
2025-06-03 02:22:37 +0100

    Update run.sh

--- spotify-connect/run.sh
+++ spotify-connect/run.sh
@@ -1,66 +1,43 @@
 #!/bin/bash
 set -e
 
-echo "[Info] Show audio device"
+echo "[Info] Show audio devices:"
 aplay -l
 
 CONFIG_PATH=/data/options.json
 
-length=$(jq --raw-output '.instances | length' $CONFIG_PATH)
+length=$(jq -r '.instances | length' "$CONFIG_PATH")
 
-for i in `seq 0 $(($length-1))`; do
+for i in $(seq 0 $((length - 1))); do
+  SPEAKER=$(jq -r ".instances[$i].speaker // \"hw:0,1\"" "$CONFIG_PATH")
+  DEVICE_TYPE=$(jq -r ".instances[$i].device_type // \"speaker\"" "$CONFIG_PATH")
+  DEVICE_NAME=$(jq -r ".instances[$i].device_name" "$CONFIG_PATH")
+  BITRATE=$(jq -r ".instances[$i].bitrate // 160" "$CONFIG_PATH")
+  ALLOW_GUESTS=$(jq -r ".instances[$i].allow_guests // true" "$CONFIG_PATH")
+  INITIAL_VOLUME=$(jq -r ".instances[$i].initial_volume // 80" "$CONFIG_PATH")
+  SPOTIFY_USER=$(jq -r ".instances[$i].spotify_user // empty" "$CONFIG_PATH")
+  SPOTIFY_PASSWORD=$(jq -r ".instances[$i].spotify_password // empty" "$CONFIG_PATH")
 
-  DISCOVERY_MODE="$(jq --raw-output ".instances[$i] .discovery_mode" $CONFIG_PATH)"
-
-  SPEAKER="$(jq --raw-output ".instances[$i] .speaker" $CONFIG_PATH)"
-  if [ "$SPEAKER" == "null" ] ; then
-    SPEAKER="hw:0,1"
+  DISCOVERY_ARG=""
+  if [ "$ALLOW_GUESTS" != "true" ]; then
+    DISCOVERY_ARG="--disable-discovery"
   fi
 
-  DEVICE_TYPE="$(jq --raw-output ".instances[$i] .device_type" $CONFIG_PATH)"
-  if [ "$DEVICE_TYPE" == "null" ] ; then
-    DEVICE_TYPE="speaker"
+  echo "[$((i+1))/$length] Starting spotify connect for device: $DEVICE_NAME"
+
+  LIBRESPOT_CMD="librespot -n \"$DEVICE_NAME\" --device \"$SPEAKER\" --bitrate $BITRATE --initial-volume $INITIAL_VOLUME --device-type $DEVICE_TYPE $DISCOVERY_ARG"
+
+  if [ -n "$SPOTIFY_USER" ] && [ -n "$SPOTIFY_PASSWORD" ]; then
+    LIBRESPOT_CMD="$LIBRESPOT_CMD -u \"$SPOTIFY_USER\" -p \"$SPOTIFY_PASSWORD\""
   fi
 
-  DEVICE_NAME="$(jq --raw-output ".instances[$i] .device_name" $CONFIG_PATH)"
-
-  BITRATE="$(jq --raw-output ".instances[$i] .bitrare" $CONFIG_PATH)"
-  if [ "$BITRATE" == "null" ] ; then
-    BITRATE="160"
-  fi
-
-  ALLOW_GUESTS=""
-  if [ "$(jq --raw-output ".instances[$i] .allow_guests" $CONFIG_PATH)" == "false" ] ; then
-    ALLOW_GUESTS="--disable-discovery"
-  fi
-
-  INITIAL_VOLUME="$(jq --raw-output ".instances[$i] .initial_volume" $CONFIG_PATH)"
-  if [ "$INITIAL_VOLUME" == "null" ] ; then
-    INITIAL_VOLUME=80
-  fi
-
-  SPOTIFY_USER="$(jq --raw-output ".instances[$i] .spotify_user" $CONFIG_PATH)"
-  SPOTIFY_PASSWORD="$(jq --raw-output ".instances[$i] .spotify_password" $CONFIG_PATH)"
-
-
-  if [ "$SPOTIFY_USER" == "null" ] || [ "$SPOTIFY_PASSWORD" == "null" ] ; then
-    echo "[$(($i+1))/$length] Starting spotify connect without account"
-    if [ $i == $(($length-1)) ] ; then
-      librespot -n "$DEVICE_NAME" --device "$SPEAKER" --bitrate "$BITRATE" --initial-volume	"$INITIAL_VOLUME" --device-type	"$DEVICE_TYPE" "$ALLOW_GUESTS"
-    else
-      librespot -n "$DEVICE_NAME" --device "$SPEAKER" --bitrate "$BITRATE" --initial-volume	"$INITIAL_VOLUME" --device-type	"$DEVICE_TYPE" "$ALLOW_GUESTS" &
-    fi
-
+  if [ "$i" -eq $((length - 1)) ]; then
+    # Last instance: exec to allow container to track process
+    eval exec $LIBRESPOT_CMD
   else
-    echo "[$(($i+1))/$length] Starting spotify with account $SPOTIFY_USER"
-    if [ $i == $(($length-1)) ] ; then
-      librespot -n "$DEVICE_NAME" -u "$SPOTIFY_USER" -p "$SPOTIFY_PASSWORD" --device "$SPEAKER" --bitrate "$BITRATE" --initial-volume	"$INITIAL_VOLUME" --device-type	"$DEVICE_TYPE"  "$ALLOW_GUESTS"
-    else
-      librespot -n "$DEVICE_NAME" -u "$SPOTIFY_USER" -p "$SPOTIFY_PASSWORD" --device "$SPEAKER" --bitrate "$BITRATE" --initial-volume	"$INITIAL_VOLUME" --device-type	"$DEVICE_TYPE"  "$ALLOW_GUESTS" &
-    fi
+    # Background others
+    eval $LIBRESPOT_CMD &
   fi
 
-  #just wait a couple seconds for better log understanding
   sleep 2
-
 done
